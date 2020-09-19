4af36b2adab63fc2d837432a5db08457bdcd85a1
Stharley Maxwell <stharleymax@gmail.com>
2020-09-19 16:52:38 -0300

    eslint configured

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express, { response } from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "GO STACK 11" });
-});
+app.get('/', (req, res) => res.json({ message: 'GO STACK 11' }));
 
 app.listen(3333, () => {
-  console.log("🚀🚀 server started on port 3333");
+  console.log('🚀🚀 server started on port 3333');
 });
