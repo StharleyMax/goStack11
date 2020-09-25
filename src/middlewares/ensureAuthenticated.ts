bc154bbe7e47849acbcb5f45c7ad24b3eb8d3032
Stharley Maxwell <stharleymax@gmail.com>
2020-09-25 11:00:30 -0300

    bug fixed, transpileOnly >>>> transpilo-only

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
+
 import authConfig from '../config/auth';
 
 interface TokenPayload {
@@ -16,21 +17,22 @@ export default function ensureAuthenticated(
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error('JTW token is missing');
+    throw new Error('JWT token is missing');
   }
 
   const [, token] = authHeader.split(' ');
+
   try {
     const decoded = verify(token, authConfig.jwt.secret);
 
     const { sub } = decoded as TokenPayload;
-
+    console.log(sub);
     request.user = {
       id: sub,
     };
 
     return next();
   } catch {
-    throw new Error('Invalid JTW token');
+    throw new Error('Invalid JWT token');
   }
 }
